cc24ca2765bae9f65c694c2ba2e7d5a08e4e50fb
Mercy-Muchai <mercy.muchai@andela.com>
2017-04-28 13:14:42 +0300

    [Chore] Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+    // plugins: [HtmlWebpackPluginConfig]
+};
