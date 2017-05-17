a053cd0fa132896a8e24e0eaa91f4e1db61e319e
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-17 11:14:56 +0300

    [feature] filter through sources using parameters provided

--- src/js/components/Sources.js
+++ src/js/components/Sources.js
@@ -2,56 +2,119 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
 
+const FILTERS = {
+  country: {
+    au: 'au',
+    de: 'de',
+    us: 'us'
+  },
+  language: {
+    en: 'en',
+    de: 'de',
+    fr: 'fr'
+  },
+  category: {
+    business: 'business',
+    entertainment: 'entertainment',
+    gaming: 'gaming',
+    general: ' general',
+    music: 'music',
+    politics: 'politics',
+    science_and_nature: 'science-and-nature',
+    sport: 'sport',
+    technology: 'technology'
+  }
+};
+
 export default class Sources extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sources: [],
-    }
+      sources: {
+        sources: []
+      },
+      articles: [],
+      currentFilter: {
+        filterKey: 'language',
+        filterValue: 'en',
+      },
+      filters: FILTERS
+    };
+    this.handleFilterChange = this.handleFilterChange.bind(this);
   }
-  componentWillMount() {
-    Axios.get('https://newsapi.org/v1/sources?language=en')
-      .then((result) => {
-        console.log('getting result: ', result);
-        this.setState({
-          sources: result.data.sources
-        });
-      })
+  componentDidMount() {
+    Axios
+      .get('https://newsapi.org/v1/sources?')
+      .then(({ data }) => {
+        this.setState((prevState) => ({
+          sources: data
+        }));
+      });
+  }
 
+  handleFilterChange(event) {
+    const { name, value } = event.target;
+
+    this.setState((prevState) => {
+      const newFilter = Object.assign({}, prevState.currentFilter, {
+        [name]: value
+      });
+      return Object.assign({}, prevState, {
+        currentFilter: newFilter
+      });
+    });
   }
-  // componentDidMount() {
-  //   Axios.get('https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=213327409d384371851777e7c7f78dfe')
-  //     .then((result) => {
-  //       console.log('getting result: ', result);
-  //       this.setState({
-  //         articles: result.data.articles
-  //       });
-  //     })
-  // }
+
   render() {
+    const {
+            filters,
+      sources: { sources },
+      currentFilter: {
+                filterKey,
+        filterValue
+            }
+        } = this.state;
+
+    const sourcesToDisplay = sources.filter(source => {
+      return source[filterKey] === filterValue;
+    });
+
     return (
-      <div>
-        <h1>Sources</h1>
-        {this.state.sources.map((sources) => (
-          <div>
-             <div>
-            <div className="card col-xs-20 col-sm-10 col-md-2 m-2" key={sources.id}>
+      <div className="container">
+        <select
+          className="form-control c-select mb-2"
+          name="filterKey"
+          onChange={this.handleFilterChange}
+          defaultValue={filterKey}
+        >
+          {Object.keys(filters).map(key => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+        <select
+          className="form-control c-select"
+          name="filterValue"
+          onChange={this.handleFilterChange}
+          defaultValue={filterValue}>
+          {Object.keys(filters[filterKey]).map(key => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+        <div className="row">
+          {sourcesToDisplay.map(source => (
+            <div className="card col-xs-12 col-sm-6 col-md-4 m-2" key={source.id}>
               <div className="card-block">
-                <h4 className="card-title"> <a target="_blank" href = {sources.url} >{sources.name}</a></h4>
-                {/*<p className="card-text">{articles.description}</p>*/}
+                <h4 className="card-title"><a target="_blank" href={source.url} >{source.name}</a></h4>
+                <p className="card-text">{source.description}</p>
               </div>
             </div>
-
-          </div>
-
-           
-
-            {/*{sources.description}*/}
-
-          </div>
-        ))
-        }
-      </div>
-    )
+          ))}
+        </div>
+      </div >
+    );
   }
 }
