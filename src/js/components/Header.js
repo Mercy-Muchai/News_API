4f79ce4109dc30ce22f809accfc9983aa8f8ed89
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-19 09:50:22 +0300

    [chore] style navbar

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,8 +4,8 @@ import Login from './Login';
 export default class Logo extends React.Component {
   render() {
     return (
-        <nav className="navbar-inverse navbar-fixed-top"style={{backgroundColor: '#ffffff', color: '#2d60c5', fontFamily: '-webkitbody', fontSize: '50px' }}>
-            <h1 id="header">
+        <nav className="navbar-inverse navbar-fixed-top">
+            <h1 className="navbar-brand">
               <img src={require("/Users/mercymuchai/Desktop/CHECKPOINT 1/cp1/src/css/img/newsicon.png")} width="60" height="60" />Get News
            <Login />
             </h1>
