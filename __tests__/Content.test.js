17d97a8e7dbdba2e6f6f53308872c51cb0a6d4da
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-23 12:07:26 +0300

    [chore] write tests for content

--- __tests__/Content.test.js
+++ __tests__/Content.test.js
@@ -3,11 +3,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import shallowToJSON from 'enzyme-to-json';
 import Header from '../src/js/components/Header';
+import Sources from '../src/js/components/Sources';
+import Articles from '../src/js/components/Articles';
+import RenderHeadlines from '../src/js/components/RenderHeadlines';
 
-describe('News API', () => {
-  it('the app should have a image', () => {
-    const app = shallow(<Header />);
-    expect(app.contains(<div>navbar-brand</div>)).toBe(false);
+describe('Test Header component', () => {
+  const wrapper = shallow(<Header/>);
+  it('should have a navbar', () => {
+    expect(wrapper.node.type).toBe('nav');
+  });
+});
+
+describe('Test Sources component', () => {
+  const wrapper = shallow(<Sources/>);
+  it('should have a div', () => {
+    expect(wrapper.node.type).toBe('div');
   });
 });
 
