601a26b7c8fd691346f0b05f0a2dc30fcbe64827
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-24 18:30:34 +0300

    [chore] complete tests

--- __tests__/Content.test.jsx
+++ __tests__/Content.test.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import { shallow, wrapper } from 'enzyme';
 import shallowToJSON from 'enzyme-to-json';
 import Sources from '../src/js/components/Sources.jsx';
-import Articles from '../src/js/components/Articles.jsx';
-import RenderHeadlines from '../src/js/components/RenderHeadlines.jsx';
-import App from '../src/js/components/App.jsx';
 
 describe('Test Sources component', () => {
   const wrapper = shallow(<Sources />);
@@ -37,24 +34,6 @@ const mockSources = {
         "top",
         "latest"
       ]
-    },
-    {
-      id: "ars-technica",
-      name: "Ars Technica",
-      description: "The PC enthusiast's resource. Power users and the tools they love, without computing religion.",
-      url: "http://arstechnica.com",
-      category: "technology",
-      language: "en",
-      country: "us",
-      urlsToLogos: {
-        small: "",
-        medium: "",
-        large: ""
-      },
-      sortBysAvailable: [
-        "top",
-        "latest"
-      ]
     },]
 }
 describe('Sources', () => {
@@ -66,6 +45,6 @@ describe('Sources', () => {
     wrapper.setState(mockSources)
     wrapper.update();
     const sources = wrapper.find('sources-container col-sm-3')
-    expect(sources.length).toBeLessThanOrEqualTo(2);
+    expect(sources.length).toBeLessThanOrEqualTo(1);
   });
 });
--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -30,6 +30,7 @@ export default class App extends React.Component {
         });
       })
   }
+  
   componentDidMount() {
     this.sortArticles()
   }
